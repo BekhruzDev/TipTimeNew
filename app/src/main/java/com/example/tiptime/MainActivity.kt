91a2a8837a18e5fec35879bcd81ab5a0a21a4bdd
BekhruzDev <bekhruzabdullakhujaev@gmail.com>
2022-03-22 23:31:23 +0500

    Error text input handling logic moved into viewModel

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,50 +7,57 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel:TipViewModel by viewModels()
-    private lateinit var binding: ActivityMainBinding //identifier of NON-PRIMITIVE TYPE to be initialized later
+    private val viewModel: TipViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.calculateButton.setOnClickListener { calculate() }
-        viewModel.formattedTip.observe(this){
-                newFormattedTip ->
+        viewModel.formattedTip.observe(this) { newFormattedTip ->
             binding.tipResult.text = getString(R.string.tip_amount, newFormattedTip)
         }
-        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->  handleKeyEvent(view, keyCode)}
+
+        //if there is an invalid input in the textField
+        viewModel.isServiceAmountInvalid.observe(this) {
+            if (viewModel.isServiceAmountInvalid.value == true) {
+                binding.costOfService.isErrorEnabled = true
+                binding.costOfService.error = "Invalid amount!"
+                binding.tipResult.isVisible = false
+            } else {
+                binding.costOfService.isErrorEnabled = false
+                binding.tipResult.isVisible = true
+            }
+        }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
+
     }
 
     private fun calculate() {
         val inputServiceAmount = binding.costOfServiceEditText.text.toString()
-        val tipPercent = when(binding.tipOptions.checkedRadioButtonId){
+        val tipPercent = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 20
             R.id.option_eighteen_percent -> 18
             else -> 15
         }
-        if(inputServiceAmount.toDoubleOrNull() == null){
-            setErrorTextField(true)
-        }else{
-            setErrorTextField(false)
-            if(binding.roundUpSwitch.isChecked){
-                viewModel.calculateTip(inputServiceAmount.toDouble(), tipPercent, true)
-            }else
-            viewModel.calculateTip(inputServiceAmount.toDouble(), tipPercent, false)
+        if (binding.roundUpSwitch.isChecked) {
+            viewModel.calculateTip(inputServiceAmount.toDoubleOrNull(), tipPercent, true)
+        } else {
+            viewModel.calculateTip(inputServiceAmount.toDoubleOrNull(), tipPercent, false)
         }
     }
 
-    private fun setErrorTextField(error:Boolean){
-        if(error){
-            binding.costOfService.isErrorEnabled = true
-            binding.costOfService.error = "Invalid amount!"
-        }
-        else{
-            binding.costOfService.isErrorEnabled = false
-        }
-    }
     private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
             // Hide the keyboard
--- app/src/main/java/com/example/tiptime/TipViewModel.kt
+++ app/src/main/java/com/example/tiptime/TipViewModel.kt
@@ -7,16 +7,24 @@ import java.text.NumberFormat
 import kotlin.math.ceil
 
 class TipViewModel : ViewModel() {
+    private val _isServiceAmountInvalid = MutableLiveData<Boolean>()
+    val isServiceAmountInvalid: LiveData<Boolean>
+        get() = _isServiceAmountInvalid
     private val _formattedTip = MutableLiveData<String>()
     val formattedTip: LiveData<String>
         get() = _formattedTip
 
-    fun calculateTip(amount: Double, percent:Int, isRounded: Boolean) {
-        var tipResult = amount * percent / 100
-        if(isRounded){
-            tipResult = ceil(tipResult)
-            _formattedTip.value = NumberFormat.getCurrencyInstance().format(tipResult)
-        }else
-         _formattedTip.value = NumberFormat.getCurrencyInstance().format(tipResult)
+    fun calculateTip(serviceAmount: Double?, percent: Int, isRounded: Boolean) {
+        if (serviceAmount == null || serviceAmount == 0.0) {
+            _isServiceAmountInvalid.value = true
+        } else {
+            _isServiceAmountInvalid.value = false
+            var tipResult = serviceAmount * percent / 100
+            if (isRounded) {
+                tipResult = ceil(tipResult)
+                _formattedTip.value = NumberFormat.getCurrencyInstance().format(tipResult)
+            } else
+                _formattedTip.value = NumberFormat.getCurrencyInstance().format(tipResult)
+        }
     }
 }
